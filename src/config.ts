58de0582c1f1af0912dbb00ceef888601c908114
p-e-t-e-r1 <980028458@qq.com>
2025-12-07 21:29:15 +0800

    修改配置

--- src/config.ts
+++ src/config.ts
@@ -3,26 +3,22 @@ import * as Updates from "expo-updates";
 import Constants from "expo-constants";
 
 //用js的好处是 更改不需要重新打包
-const decodeKey = (parts: string[]) => parts.join("");
-
 export const getConfig = () => {
   const productionConfig = {
     ENV: "production",
     API_URL: "https://y.yoozyai.com",
 
-    TOS_ACCESS_KEY_ID: decodeKey(["AKL", "TNGUxNTUwYmEyODQwNGU1MjgzMzcwNDFiZDEyZWU4MGU"]),
-    TOS_ACCESS_KEY_SECRET: decodeKey([
-      "TVRSallqQmlNR1EwTnpkbE5HSmlNemczWkRBMU9UWmlaRGcwTVRFMk5qVQ",
-      "==",
-    ]),
+    TOS_ACCESS_KEY_ID: "AKLTNGUxNTUwYmEyODQwNGU1MjgzMzcwNDFiZDEyZWU4MGU",
+    TOS_ACCESS_KEY_SECRET:
+      "TVRSallqQmlNR1EwTnpkbE5HSmlNemczWkRBMU9UWmlaRGcwTVRFMk5qVQ==",
     TOS_REGION: "cn-shanghai",
     TOS_ENDPOINT: "tos-cn-shanghai.volces.com",
     TOS_BUCKET: "yoozyres",
 
     STORAGE: "oss",
 
-    OSS_ACCESSKEY: decodeKey(["LTAI", "5t7zcYu63DW8JrCNYRwc"]),
-    OSS_ACCESSSECRET: decodeKey(["yR6RznPnoCltvPQSUg8pvBca6YdML", "5"]),
+    OSS_ACCESSKEY: "LTAI5t7zcYu63DW8JrCNYRwc",
+    OSS_ACCESSSECRET: "yR6RznPnoCltvPQSUg8pvBca6YdML5",
     OSS_BUCKET: "yoozy",
     OSS_REGION: "oss-cn-hangzhou",
     OSS_ENDPOINT: "https://oss-cn-hangzhou.aliyuncs.com",
@@ -36,20 +32,8 @@ export const getConfig = () => {
     // BUCKET: 'veogoresources'
     // ENDPOINT: "https://veogoresources.s3.ap-southeast-1.amazonaws.com"
 
-    ACCESSKEY: decodeKey(["A", "KI", "A", "X", "JQ", "Y", "B", "N", "K", "M", "H", "P", "X", "6", "P", "3", "P", "J"]),
-    ACCESSSECRET: decodeKey([
-      "f",
-      "0",
-      "R",
-      "b",
-      "F",
-      "O",
-      "A",
-      "B",
-      "chwFwyv6LqVOcTOnVYGOIRrX6hA/q4",
-      "O",
-      "V",
-    ]),
+    ACCESSKEY: "AKIAXJQYBNKMHPX6P3PJ",
+    ACCESSSECRET: "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",
     REGION: "cn-north-1",
     BUCKET: "veogoresources",
     ENDPOINT: "https://veogoresources.s3.cn-north-1.amazonaws.com.cn",
